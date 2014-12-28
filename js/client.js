66fd00a07a55daea730d7b6433095080aca67334
Brian Oldak <broldak@gmail.com>
2014-12-28 02:23:53 -0500

    socket.io chat init

---
+++ js/client.js
@@ -0,0 +1,52 @@
+var socket = io();
+
+$('#nick-input').focus();
+
+$('#msg-input-form').on('submit', function(){
+  socket.emit('chat message', $('#msg-input').val());
+  $('#msg-input').val('');
+  return false;
+});
+
+$('#nickname-form').on('submit', function(){
+  socket.emit('set nickname', $('#nick-input').val());
+  $('#nickname-form').fadeOut(function(){
+    $('#msg-input-form').fadeIn();
+    $('#msg-input').focus();
+  });
+  return false;
+});
+
+socket.on('users', function(data){
+  if(data.length != 0){
+    $('#users').text('');
+  }
+
+  data.forEach(function(elem, index, arr){
+    $('#users').append($('<li class="user">').text(elem));
+  });
+});
+
+socket.on('connected', function(data){
+  var nick = data.nick;
+  var users = data.users;
+
+  $('#users').text('');
+  $('#messages').append($('<li class="chat-msg">').text(nick));
+
+  users.forEach(function(elem, index, arr){
+    $('#users').append($('<li class="user">').text(elem));
+  });
+});
+
+socket.on('chat message', function(data){
+  var nick = data.nickname;
+  var msg = data.message;
+
+  var text = '<span class="msg-nick">'+nick+': ';
+  text = text+'</span><span class="msg-text">'+msg+'</span>';
+
+  $('#messages').append($('<li class="chat-msg">').html(text));
+  $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  $('#msg-input').focus();
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var userList = [];
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  nickSet = false;
+  console.log('a user connected');
+
+  io.emit('users', userList);
+
+  socket.on('set nickname', function(nickname){
+    socket.nickname = nickname;
+    nickSet = true;
+
+    userList.push(socket.nickname);
+    io.emit('connected', {nick: socket.nickname +' connected.',
+                                        users: userList});
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', {nickname: socket.nickname, 
+                             message: msg});
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
